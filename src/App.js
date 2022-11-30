7702c758de293e049003ca5397c043176f14dce8
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-11-30 12:49:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import "./App.css";
+import { increment, decrement, incrementByAmount } from "./redux/counterSlice";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    const [incrementAmount, setIncrementAmount] = useState(2);
+
+    return (
+        <div className="App">
+            <h1>count: {count}</h1>
+            <input
+                onChange={(e) => setIncrementAmount(e.target.value)}
+                value={incrementAmount}
+            />
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button
+                onClick={() =>
+                    dispatch(incrementByAmount(Number(incrementAmount)))
+                }
+            >
+                Add
+            </button>
+        </div>
+    );
 }
 
 export default App;
